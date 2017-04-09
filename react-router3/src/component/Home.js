9516b963e3156f5a7ea11c99b93401271e83a7ea
森田賢二 <kenjimoritata@gmail.com>
2017-04-09 11:07:49 +0900

    same project around routing

--- react-router3/src/component/Home.js
+++ react-router3/src/component/Home.js
@@ -1,5 +1,16 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router';
 
- const Home = () =>  <div> <h1>Home</h1> </div>
+class Home  extends Component {
+ render(){
+    return (
+        <div>
+            <h1>Home</h1>
+            home here!!
+
+        </div>
+    )
+  }
+}
 
 export default Home
--- react-router3/src/index.js
+++ react-router3/src/index.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import {browserHistory, IndexRoute, Router, Route, NotFoundRoute, DefaultRoute, Link, RouteHandler} from 'react-router';
 import App from './component/App';
+import {browserHistory, IndexRoute, Redirect, Router, Route, NotFoundRoute, DefaultRoute, Link, RouteHandler} from 'react-router';
+import Home from './component/Home';
+import About from './component/About';
+import Features from './component/Features';
 import 'babel-polyfill';
 
+let createElement = function(Component, props) {
+    props.context = {user: 'kenji', isMenber: true};
+    return <Component {...props} />;
+};
+
+
+class Endpoint extends Component {
+ render(){
+  return (
+      <Router history={browserHistory} createElement={createElement} >
+        <Route path='/home/' component={App}>
+            <IndexRoute component={Home} />
+            <Route path='about'component={About} />
+            <Route path='feature' component={Features} />
+        </Route>
+        <Redirect from='/home' to='/home/' />
+      </Router>
+    )
+ }
+}
 window.addEventListener('DOMContentLoaded', ()=>{
- ReactDOM.render(<App />, document.querySelector('main'));
+ ReactDOM.render(<Endpoint />, document.querySelector('main'));
 })
 
