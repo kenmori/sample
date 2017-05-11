fde65ae8e4bb884b5df972dbfbce9aac461f63df
森田賢二 <kenjimoritata@gmail.com>
2017-05-11 21:37:37 +0900

    react-router4

--- react-router4/src/component/Home.js
+++ react-router4/src/component/Home.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Link} from 'react-router-dom';
 
 class Home  extends Component {
  render(){
+     console.log(this.props)
     return (
         <div>
             <h1>Home</h1>
