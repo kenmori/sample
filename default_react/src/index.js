39d2f9806f29b68eb9d5f6116ff161fbfb5dc687
森田賢二 <kenjimoritata@gmail.com>
2017-06-26 20:22:07 +0900

    add react-flux-default

--- default_react/src/index.js
+++ default_react/src/index.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import 'babel-polyfill';
+import 'lodash';
 
 
 class Greet extends Component {
  render() {
+  let enrollment = [
+   {enrollment: 2, grade: 100},
+   {enrollment: 2, grade: 80},
+   {enrollment: 1, grade: 89},
+  ];
+
+  //
+  // console.log(enrollment.filter(elem => elem.enrollment >= 2).reduce((elem){
+  //       return elem.grade / elem.length});
+  //  );
+  console.log(_.chain);
+
+
   return (
    <ul id='result'></ul>
   )
