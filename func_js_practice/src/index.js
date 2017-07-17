52856beef2fbb1bb57d55e0589285ce022f4c317
森田賢二 <kenjimoritata@gmail.com>
2017-07-17 17:51:06 +0900

    add seq

--- func_js_practice/src/index.js
+++ func_js_practice/src/index.js
@@ -5,8 +5,27 @@ import 'babel-polyfill';
 
 class Greet extends Component {
  render() {
+  ////////////////////////
+  //値に対して複数の関数をマッピングするseq関数
+  const seq = function(){
+   const funcs = Array.prototype.slice.call(arguments);
+   console.log(funcs);
+   return function(val){
+    funcs.forEach(
+        (fun)=> {
+          console.log(fun(val));
+        }
+    )
+   }
+  }
+  const add = (a) => a + 3;
+  const div = (a) => a - 4;
+  const seqed = seq(add, div);
+  ////////////////////////
+
+
   return (
-   <ul id='result'>fa</ul>
+   <ul id='result'>fafafa</ul>
   )
  }
 }
