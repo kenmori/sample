16ece77c42518328e0281fc20e2232e51872a3cb
森田賢二 <kenjimoritata@gmail.com>
2017-08-11 11:04:21 +0900

    Add functor

--- func_js_practice/src/Wrapper.js
+++ func_js_practice/src/Wrapper.js
@@ -1,3 +1,4 @@
+import R from 'ramda';
 //値をラッピングするための関数データ型
 export default class Wrapper {
     constructor(value){
@@ -10,8 +11,22 @@ export default class Wrapper {
     toString() {
         return 'Wrapper (' + this._value + ')';
     }
+    fmap(f){
+        return wrap(f(this._value));
+    }
+
 }
 //wrap :: A -> Wrapper(A)
 export const wrap = function(val){ return new Wrapper(val)}
 
+const curryed = R.curry((a, b)=> a + b);
 
+console.log(curryed, "fafa");
+
+var pluse3 = curryed(3);
+
+console.log(pluse3(4), 'pluse3 anserwer');
+
+
+
+console.log(wrap(3).fmap(pluse3).fmap(pluse3).map(R.identity));
